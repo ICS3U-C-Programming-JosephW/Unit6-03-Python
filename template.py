5f1c622a11c45b18f2982edf3a7298812c1db4d6
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-05-24 19:33:51 -0400

    Initial commit

---
+++ template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Month Day, Year
+# Add something.
+
+
+# Define the main function.
+def main():
+    pass
+
+
+# Check if the special name of the file is __main__.
+if __name__ == "__main__":
+    # Run the main function if so.
+    main()
